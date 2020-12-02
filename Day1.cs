64676c31e98d5dec31523c12cbd517408ef25783
Chad Nedzlek <chadnedz@microsoft.com>
2020-12-01 20:45:53 -0800

    Day 1

---
+++ Day1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    public class Day1
+    {
+        public static async Task Problem1()
+        {
+            HashSet<int> value = new HashSet<int>();
+            foreach (string line in await Data.GetDataLines(1,1))
+            {
+                var number = int.Parse(line);
+                int needed = 2020 - number;
+                if (value.Contains(needed))
+                {
+                    Console.WriteLine($"Found:  {number} * {needed} = {number * needed}");
+                    return;
+                }
+
+                value.Add(number);
+            }
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Failed to locate answer");
+            Console.ResetColor();
+        }
+
+        public static async Task Problem2()
+        {
+            HashSet<int> seen = new HashSet<int>();
+            Dictionary<int, (int,int)> pairs = new Dictionary<int, (int, int)>();
+            foreach (string line in await Data.GetDataLines(1,1))
+            {
+                var number = int.Parse(line);
+                int needed = 2020 - number;
+                if (pairs.TryGetValue(needed, out var pair))
+                {
+                    Console.WriteLine($"Found: {pair.Item1} * {pair.Item2} * {number} = {number * pair.Item1 * pair.Item2}");
+                    return;
+                }
+
+                foreach (var a in seen)
+                {
+                    var partialSum = a + number;
+                    if (partialSum < 2020)
+                    {
+                        pairs.TryAdd(a + number, (a, number));
+                    }
+                }
+
+                seen.Add(number);
+            }
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Failed to locate answer");
+            Console.ResetColor();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await Day1.Problem2();
+        }
+    }
+
+    public class Data
+    {
+        public static Task<string[]> GetDataLines(int day, int problem)
+        {
+            return File.ReadAllLinesAsync(
+                Path.Combine(
+                    Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
+                    "data",
+                    $"day{day}_problem{problem}.txt"
+                )
+            );
+        }
+    }
+}
