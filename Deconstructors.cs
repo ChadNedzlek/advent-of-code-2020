127b5994b905d1aa5327300350d6dc042802425a
Chad Nedzlek <chadnedz@microsoft.com>
2020-12-01 21:12:45 -0800

    A few more deconstructors

--- Deconstructors.cs
+++ Deconstructors.cs
@@ -5,6 +5,54 @@ namespace AdventOfCode
 {
     public static class Deconstructors
     {
+        public static void Deconstruct(this Match match, out string a)
+        {
+            if (!match.Success)
+            {
+                throw new ArgumentException("Match is not successful");
+            }
+
+            if (match.Groups.Count != 2)
+            {
+                throw new ArgumentException("Match does not have 1 groups");
+            }
+
+            a = match.Groups[1].Value;
+        }
+
+        public static void Deconstruct(this Match match, out string a, out string b)
+        {
+            if (!match.Success)
+            {
+                throw new ArgumentException("Match is not successful");
+            }
+
+            if (match.Groups.Count != 5)
+            {
+                throw new ArgumentException("Match does not have 2 groups");
+            }
+
+            a = match.Groups[1].Value;
+            b = match.Groups[2].Value;
+        }
+
+        public static void Deconstruct(this Match match, out string a, out string b, out string c)
+        {
+            if (!match.Success)
+            {
+                throw new ArgumentException("Match is not successful");
+            }
+
+            if (match.Groups.Count != 5)
+            {
+                throw new ArgumentException("Match does not have 4 groups");
+            }
+
+            a = match.Groups[1].Value;
+            b = match.Groups[2].Value;
+            c = match.Groups[3].Value;
+        }
+
         public static void Deconstruct(this Match match, out string a, out string b, out string c, out string d)
         {
             if (!match.Success)
@@ -21,6 +69,25 @@ namespace AdventOfCode
             b = match.Groups[2].Value;
             c = match.Groups[3].Value;
             d = match.Groups[4].Value;
+        }
+
+        public static void Deconstruct(this Match match, out string a, out string b, out string c, out string d, out string e)
+        {
+            if (!match.Success)
+            {
+                throw new ArgumentException("Match is not successful");
+            }
+
+            if (match.Groups.Count != 6)
+            {
+                throw new ArgumentException("Match does not have 5 groups");
+            }
+
+            a = match.Groups[1].Value;
+            b = match.Groups[2].Value;
+            c = match.Groups[3].Value;
+            d = match.Groups[4].Value;
+            e = match.Groups[5].Value;
         }
     }
 }
