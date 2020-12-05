ff311df02810ca03755eccffe91c3076476fa5d8
Chad Nedzlek <chadnedz@microsoft.com>
2020-12-04 22:54:17 -0800

    Day 5

---
+++ Day5.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    public class Day5
+    {
+        private static int SeatId(int row, int seat) => row * 8 + seat;
+
+        public static async Task Problem1()
+        {
+            var data = await Data.GetDataLines(5, 1);
+            int seatId = 0;
+            foreach (var line in data)
+            {
+                var row = Convert.ToInt32(line.Substring(0, 7).Replace('F','0').Replace('B','1'), 2);
+                var seat = Convert.ToInt32(line.Substring(7, 3).Replace('L','0').Replace('R','1'), 2);
+
+                seatId = Math.Max(seatId, SeatId(row, seat));
+            }
+
+            Console.WriteLine($"Silly format highest seat: {seatId}");
+        }
+
+        public static async Task Problem2()
+        {
+            var data = await Data.GetDataLines(5, 1);
+            var set = new HashSet<int>(Enumerable.Range(0, 128*8));
+            foreach (var line in data)
+            {
+                var row = Convert.ToInt32(line.Substring(0, 7).Replace('F','0').Replace('B','1'), 2);
+                var seat = Convert.ToInt32(line.Substring(7, 3).Replace('L','0').Replace('R','1'), 2);
+                set.Remove(SeatId(row, seat));
+            }
+            
+            for (int i = 0; set.Contains(i); i++)
+            {
+                set.Remove(i);
+            }
+
+            for (int i = 933; set.Contains(i); i--)
+            {
+                set.Remove(i);
+            }
+
+            Console.WriteLine($"Silly format missing seat: {set.First()}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,14 +1,44 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 
 namespace AdventOfCode
 {
-    class Program
+    internal class Program
     {
-        static async Task Main(string[] args)
+        private static async Task Main(string[] args)
         {
-            await Day4.Problem2();
+            IEnumerable<Type> dayTypes =
+                Assembly.GetExecutingAssembly().GetTypes().Where(t => t.Name.StartsWith("Day"));
+
+            bool all = args.Length > 0;
+
+            IOrderedEnumerable<Type> orderedDays = dayTypes.OrderBy(d => int.Parse(d.Name.Substring(3)));
+            if (all)
+            {
+                foreach (Type day in orderedDays)
+                {
+                    Console.WriteLine($" ************* {day.Name}");
+                    IEnumerable<MethodInfo> methods = day.GetMethods(BindingFlags.Static | BindingFlags.Public)
+                        .Where(m => m.Name.StartsWith("Problem"));
+                    foreach (MethodInfo method in methods.OrderBy(m => m.Name))
+                    {
+                        Console.WriteLine($" ** {method.Name}");
+                        await (Task) method.Invoke(null, null);
+                    }
+                }
+            }
+            else
+            {
+                Type day = orderedDays.Last();
+                IEnumerable<MethodInfo> methods = day.GetMethods(BindingFlags.Static | BindingFlags.Public)
+                    .Where(m => m.Name.StartsWith("Problem"));
+                MethodInfo method = methods.OrderBy(m => m.Name).Last();
+                await (Task) method.Invoke(null, null);
+            }
         }
     }
 
